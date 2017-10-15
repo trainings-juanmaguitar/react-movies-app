352c97933946cb129c087ba9970bd9ae90e13bb3
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-10-15 12:21:19 +0200

    year release

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,21 +6,28 @@ import './MoviesList.css'
 const srcImgError = "http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png"
 
 const MoviesList = props => {
+  
   return (
     <div className="MoviesList">
         {
-          props.movies.map(movie => (
-            <Thumbnail 
-              key={ movie.id } 
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-              alt={ movie.title }
-              className="MoviesList__thumbnail"
-              onError={ e => { e.target.src = srcImgError } }
-            >
-              <h3>{ movie.title }</h3>
-              <p>{ movie.overview.substring(0,100) + '...' }</p>
-            </Thumbnail>
-          ))
+          props.movies.map(movie => {
+            const { id, title, poster_path, overview, release_date } = movie
+            const yearRelease = release_date.replace(/([0-9]{4})-.*/, "$1");
+            const shortDescription = overview.substring(0,100) + '...'
+            const urlImage = `https://image.tmdb.org/t/p/w500${poster_path}`
+            return (
+              <Thumbnail 
+                key={ id } 
+                src={ urlImage } 
+                alt={ title }
+                className="MoviesList__thumbnail"
+                onError={ e => { e.target.src = srcImgError } }
+              >
+                <h3>{ title } <small>{ yearRelease }</small></h3>
+                <p>{ shortDescription }</p>
+              </Thumbnail>
+            )
+          })
         }
     </div>
   );
